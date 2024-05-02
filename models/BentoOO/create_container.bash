782d9a0ac386c06b5953bea9c8c7622b968912aa
glenn <glace@OnFire>
2024-05-02 11:55:07 +0200

    changes

--- models/BentoOO/create_container.bash
+++ models/BentoOO/create_container.bash
@@ -1,10 +1,5 @@
+python training.py
 bentoml build .
 bentoml containerize animal_classifier:latest
-
-# This part is not yet correc
-docker tag 617db0264d1a bethagle/animal_classifier:latest
+docker tag "animal_classifier:$(docker images --format "{{.Tag}}" | head -n 1)" bethagle/animal_classifier:latest
 docker push bethagle/animal_classifier:latest
-
-models:
-  - "cd-classifier:latest"
-  - tag: "cd-classifier:latest"
--- models/BentoOO/training.py
+++ models/BentoOO/training.py
@@ -7,6 +7,7 @@ from preprocess import create_loaders
 import torch
 import bentoml
 import os
+import wandb
 
 
 def setup_training(root_path, image_size, batch_size, nworkers, epochs,
@@ -18,18 +19,17 @@ def setup_training(root_path, image_size, batch_size, nworkers, epochs,
         checkpoint_callback = ModelCheckpoint(monitor='Valid f1', mode="max",
                                             verbose=False,
                                             save_weights_only=False, filename="MobileNet")
-        trainer = lt.Trainer(max_epochs=epochs, logger=TensorBoardLogger("MobileNet"),
+        trainer = lt.Trainer(max_epochs=epochs, logger=WandbLogger(project="MLOPS", name="mobilenet"),
                             callbacks=[checkpoint_callback])
         model = MobileNet()
+
         trainer.fit(model=model, train_dataloaders=train_loader,
                     val_dataloaders=valid_loader)
 
     ROOT_CHECKPOINT_PATH = "../MobileNet/lightning_logs"
     latest_verison = sorted(os.listdir(ROOT_CHECKPOINT_PATH))[-1]
     checkpoint_path = os.path.join(os.path.join(ROOT_CHECKPOINT_PATH, latest_verison), "checkpoints/MobileNet.ckpt")
-    #check_point = torch.load(checkpoint_path)
     model = MobileNet.load_from_checkpoint(checkpoint_path=checkpoint_path).cpu()
-    #check_point = torch.load(checkpoint_path)
     bentoml.picklable_model.save_model("CD-Classifier", model=model)
 
 
